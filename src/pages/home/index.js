98cdf42df68f58c7a250a1dbce7c9ee966eb1ce9
Matheus-Gr <matheus.gr@aluno.ufop.edu.br>
2020-07-31 14:54:45 -0300

    v0.5.5

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -10,18 +10,20 @@ import face from '../../assets/img/willsmith.png';
 
 function Home() {
   const [dadosIniciais, setDadosIniciais] = useState([]);
-
-  useEffect(() => {
-    // http://localhost:8080/categorias?_embed=videos
-    categoriasRepository.getAllWithVideos()
-      .then((categoriasComVideos) => {
-        console.log(categoriasComVideos[0].videos[0]);
-        setDadosIniciais(categoriasComVideos);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
+  setTimeout(
+    useEffect(() => {
+      // http://localhost:8080/categorias?_embed=videos
+      categoriasRepository.getAllWithVideos()
+        .then((categoriasComVideos) => {
+          console.log(categoriasComVideos[0].videos[0]);
+          setDadosIniciais(categoriasComVideos);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }, []), 10 * 1000
+  )
+  
 
   return (
     <div style={{ background: '#141414' }}>
